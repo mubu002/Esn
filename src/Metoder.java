89e4b2e6bc3cf868f8d5036b475cc8555f218171
mubu002@student.kristiania.no <mubu002@student.kristiania.no>
2021-09-04 16:51:49 +0200

    Sanan ne aminkaoyim

--- src/Metoder.java
+++ src/Metoder.java
@@ -1,10 +1,14 @@
 public class Metoder {
     static String toRoman(int i){
+        String romedigits = "null";
         if (i == 2){
-            return "II";
-        }else{
-            return "I";
+            romedigits = "II";
+
+        }if(i == 1){
+            romedigits = "I";
+
         }
+        return romedigits;
         }
 
 }
